12f127079be50c8fde1dcceb08d96a635bf84acd
Russell Hayes <Russell Hayes>
2018-06-21 21:57:12 -0700

    Added files

--- src/api/users/users.resolvers.js
+++ src/api/users/users.resolvers.js
@@ -1,9 +1,9 @@
-const User = async (_, { id }, ctx) => {
+const user = async (_, { id }, ctx) => {
   return await ctx.models.User.findOne({ _id: id });
 };
 
 module.exports = {
   Query: {
-    User,
+    user,
   },
 };
