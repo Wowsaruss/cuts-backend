875361cf29cc05df3c8c0510d02e8df9251e333e
Russell Hayes <Russell Hayes>
2018-06-21 20:15:31 -0700

    building users graphql

--- src/api/users/users.model.js
+++ src/api/users/users.model.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const User = new Schema({});
+const User = new Schema(
+  {
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String },
+    gender: { type: String },
+    phoneNumber: { type: String },
+  },
+  { timestamps: { createdAt: 'created_at' } },
+  { timestamps: { updatedAt: 'updated_at' } }
+);
 module.exports = mongoose.model('User', User);
